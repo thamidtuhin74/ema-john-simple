5717715465f86e377d5be627c9b5cd03e2ec265e
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2023-03-29 21:44:50 +0600

    Adding Icon & showing total items

--- src/component/Shop/Shop.jsx
+++ src/component/Shop/Shop.jsx
@@ -1,15 +1,23 @@
 import React, { useEffect, useState } from 'react';
 import SingleProduct from '../SingleProduct/SingleProduct';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash ,faArrowRight } from '@fortawesome/free-solid-svg-icons'
 import './Shop.css'
 const Shop = () => {
 
     const [products, setProducts] = useState([]);
+    const[cart, setCart] = useState([]);
 
     useEffect(()=>{
         fetch('products.json')
         .then(res => res.json())
         .then(data => setProducts(data))
     },[])
+    const handleAddToCart = (product) =>{
+        // console.log(product);
+        const newCart = [...cart,product];
+        setCart(newCart);
+    }
 
     return (
         <div className='shop-container'>
@@ -19,11 +27,19 @@ const Shop = () => {
                     products.map(product => <SingleProduct
                         key = {product.id}
                         product={product}
+                        handleAddToCart = {handleAddToCart}
                         ></SingleProduct>)
                 }
             </div>
             <div className="order-summary">
                 <h3>Order Summary</h3>
+                <p>Selected Item : {cart.length}</p>
+                <p>Total Price: $1140</p>
+                <p>Total Shipping Charge: $5</p>
+                <p>Tax: $114</p>
+                <h3>Grand Total: $1559</h3>
+                <button>Clear Cart<FontAwesomeIcon icon={faTrash} /></button>
+                <button>Review Cart<FontAwesomeIcon icon={faArrowRight} /></button>
             </div>
         </div>
     );
--- src/component/SingleProduct/SingleProduct.jsx
+++ src/component/SingleProduct/SingleProduct.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import './SingleProduct.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
 
 const SingleProduct = (props) => {
 
+    const handleAddToCart = props.handleAddToCart;
+    
     const {name, price , seller, ratings, img} = props.product;
     return (
         <div className='single-product'>
@@ -13,7 +17,10 @@ const SingleProduct = (props) => {
                 <p>Manufacture : {seller}</p>
                 <p>Ratings : {ratings}</p>
             </div>
-            <button className='btn-addToCart'>Add to Cart</button>
+            <button className='btn-addToCart' onClick={()=>handleAddToCart(props.product)}>
+                Add to Cart
+                <FontAwesomeIcon icon={faCartShopping} />
+                </button>
         </div>
     );
 };
